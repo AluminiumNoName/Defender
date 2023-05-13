eaed49931edce8825a7daa600cfcda14ad5ff860
AluminiumNoName <126279236+AluminiumNoName@users.noreply.github.com>
2023-05-13 16:58:11 +0300

    Add files via upload

--- Corn.cs
+++ Corn.cs
@@ -6,17 +6,43 @@ public class Corn : MonoBehaviour
 {
     public static Corn singleton;
     public int health;
+    //4lesson
+    public int crystals;
+
+    public int basicHealth;
+    public int healthPerUpgrade;
 
     private void Awake()
     {
         singleton = this;
+        //4lesson
+        int healthBonus = healthPerUpgrade*PlayerPrefs.GetInt("healthGrade",0);
+        health = basicHealth+healthBonus;
+        crystals = PlayerPrefs.GetInt("crystals", 0);
     }
 
     public void TakeDamage()
     {
+        if (health > 0)
+        {
         health -= 1;
+
+        }
+    }
+    //4lesson
+    public void AddCrystals(int count)
+    {
+        crystals += count;
+        GameSaver.SaveCrystals();
+    }
+    //4lesson
+    public void TakeCrystals(int count)
+    {
+        crystals -= count;
+        GameSaver.SaveCrystals();
     }
 
 }
+
 
 
